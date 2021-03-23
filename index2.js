4bc2da4fbc12dcabf9afb26177e644b31fe4cf2c
Burakkepuc <41537761+Burakkepuc@users.noreply.github.com>
2021-03-23 14:44:31 +0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -11,7 +11,7 @@ if(randomNumber1 > randomNumber2){
   document.querySelector("h1").innerHTML = "Player 1 win! 🚩";
 }
   else if(randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 win! 🚩"
+    document.querySelector("h1").innerHTML = "🚩Player 2 win! "
   }
   else{
     document.querySelector("h1").innerHTML = "Draw !";
